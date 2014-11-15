1a95a29b092c5b823fcc4d933d712de0601c627c
Michael Martin Moro <sexymimi64@gmail.com>
2014-11-15 19:33:43 +0000

    removed useless imports from QueryBuilder.java

--- src/aggregator/db/QueryBuilder.java
+++ src/aggregator/db/QueryBuilder.java
@@ -3,10 +3,8 @@ package aggregator.db;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import aggregator.StringUtils;
 
